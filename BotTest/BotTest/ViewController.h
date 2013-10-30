6cd211a41af2446f9021fc68385fcef72142f1d9
Zaki Shaheen <zaki.shaheen@coeus-solutions.de>
2013-10-30 19:35:50 +0500

    created bot on xcode

---
+++ BotTest/BotTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BotTest
+//
+//  Created by Zaki Shaheen on 30/10/2013.
+//  Copyright (c) 2013 Zaki Shaheen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BotTest/BotTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BotTest
+//
+//  Created by Zaki Shaheen on 30/10/2013.
+//  Copyright (c) 2013 Zaki Shaheen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
